8f5cb3a797cf2995b99d42d45c62c2cf0b683295
xiejibing <33129072+xiejibing@users.noreply.github.com>
2025-05-12 14:01:39 +0800

    update onnxruntim_backend git url

--- build.py
+++ build.py
@@ -2681,6 +2681,8 @@ if __name__ == "__main__":
             # If armnn_tflite backend, source from external repo for git clone
             if be == "armnn_tflite":
                 github_organization = "https://gitlab.com/arm-research/smarter/"
+            elif be in ["onnxruntime"]:
+                github_organization = "https://github.com/xiejibing"
             else:
                 github_organization = FLAGS.github_organization
 
