6e31868f7b811a7db42ea74297bbda643dd01071
xiejibing <33129072+xiejibing@users.noreply.github.com>
2025-05-12 17:25:58 +0800

    use custom git repo for FIL in build.py

--- build.py
+++ build.py
@@ -2681,7 +2681,7 @@ if __name__ == "__main__":
             # If armnn_tflite backend, source from external repo for git clone
             if be == "armnn_tflite":
                 github_organization = "https://gitlab.com/arm-research/smarter/"
-            elif be in ["onnxruntime"]:
+            elif be in ["onnxruntime", "fil"]:
                 github_organization = "https://github.com/xiejibing"
             else:
                 github_organization = FLAGS.github_organization
